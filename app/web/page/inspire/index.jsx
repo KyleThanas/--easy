37271fa18cc32f33d95cf0836336917359d07e0e
zhangxiaozheng <xiaozheng.zhang@niu.com>
2020-12-20 15:01:10 +0800

    完整版

--- app/web/page/inspire/index.jsx
+++ app/web/page/inspire/index.jsx
@@ -1,7 +1,8 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import KeplerMapContainer from './widgets/keplerMap'
 import store from './store/store';
+import './index.css';
 
 export default class ListIndex extends Component {
   render() {
